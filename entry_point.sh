45bd3c705d330d585ed11d70bf1a54f960b56757
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-11 12:26:03 -0700

    - if GLOBUS_CREATE_STATE_COPY=1, state of globus is copied to /globus_state  after config.  - Otherwise, if the state file exists, the state is restored.

--- entry_point.sh
+++ entry_point.sh
@@ -12,9 +12,25 @@
 #  the server runs.
 # GLOBUS_ACTIVATE_PASSWORD password of the GLOBUS_ACTIVATE_USER.
 
+globus_state_file_name="globus_state.tar.gz"
+globus_state_dir="/globus_state"
+globus_state_file="${globus_state_dir}/${globus_state_file_name}"
+
 echo "Creating activation user"
 useradd -ms /bin/bash "${GLOBUS_ACTIVATE_USER}"
 echo ""${GLOBUS_ACTIVATE_USER}":"${GLOBUS_ACTIVATE_PASSWORD}"" | chpasswd
 echo "Configuring globus"
+if [ "$GLOBUS_CREATE_STATE_COPY" != 1 ] &&  [ -f "$globus_state_file" ]
+then
+  echo "Restoring Globus state"
+  tar -xvzf "${globus_state_file}"
+fi
 globus-connect-server-setup
+if [ "$GLOBUS_CREATE_STATE_COPY" = 1 ]
+then
+  echo "Making copy of globus state, copying into ${globus_state_file}"
+  tar -cvzf "${globus_state_file}" /var/lib/globus  \
+/var/lib/globus-connect-server /var/lib/myproxy /var/lib/myproxy-oauth
+fi
+echo "All done... Server running"
 sleep infinity
