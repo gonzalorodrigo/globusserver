d58d7d64ad2b70a54b57de9c295b305a56dca170
Gpmzañp Rpdrogp <gprodrigoalvarez@lbl.gov>
2017-11-20 11:13:13 -0800

    Data dir in the container is configurable

--- example_start_server.sh
+++ example_start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Example script to run globus connect server.
+#
+
 export GLOBUS_USER="globus_user_witho_@globusid.org"
 export GLOBUS_PASSWORD="globus_password"
 export SHORT_HOSTNAME="thehostname"
@@ -6,8 +10,9 @@ export PUBLIC_HOSTNAME="thehostname.domain.org"
 export GLOBUS_ACTIVATE_USER="activate_user"
 export GLOBUS_ACTIVATE_PASSWORD="activate_password"
 export DOMAIN_NAME="domain.org"
-export GLOBUS_DATA_DIR="./data"
+export HOST_GLOBUS_DATA_DIR="./data"
+export GUEST_GLOBUS_DATA_DIR="/data"
 export GLOBUS_PERSISTENT=1
 
 docker-compose build
-docker-compose up
+docker-compose up -d
